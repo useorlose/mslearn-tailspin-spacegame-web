a870f6756065d012641f00399b27fb88a9765a4e
Jyothsna <jyothsnakjntuh@gmail.com>
2020-10-22 12:15:39 +0530

    changes done

--- Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
+++ Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
@@ -34,10 +34,22 @@ namespace TailSpin.SpaceGame.Web
         /// The task result contains the retrieved item.
         /// </returns>
         /// <param name="id">The identifier of the item to retrieve.</param>
-        public Task<T> GetItemAsync(string id)
-        {
-            return Task<T>.FromResult(_items.Single(item => item.Id == id));
-        }
+      public Task<IEnumerable<T>> GetItemsAsync(
+    Expression<Func<T, bool>> queryPredicate,
+    Expression<Func<T, int>> orderDescendingPredicate,
+    int page = 1, int pageSize = 10
+)
+{
+    var result = _items.AsQueryable()
+        .Where(queryPredicate) // filter
+        .OrderByDescending(orderDescendingPredicate) // sort
+        .Skip(page * pageSize) // find page
+        .Take(pageSize) // take items
+        .AsEnumerable(); // make enumeratable
+
+    return Task<IEnumerable<T>>.FromResult(result);
+}
+
 
         /// <summary>
         /// Retrieves items from the store that match the given query predicate.
